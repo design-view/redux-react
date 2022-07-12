f18b4897291beba6eb6332c8ae54f4d048f6ac25
pink <pink@naver.com>
2022-07-12 14:04:33 +0900

    카운트 컴포넌트 구현완료

---
+++ src/components/CounterContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increase, decrease, setDiff } from '../modules/counter';
+import Counter from './Counter';
+
+const CounterContainer = () => {
+    //useSelector는 리덕스 스토어의 상태를 조회하는 Hook함수
+    //store.getState() 할때의 결과 동일함
+    const { number, diff } = useSelector(state => state.counter)
+    //{number: 0, diff: 1}
+    //useDispatch는 리덕스 스토어의 dispatch를 함수에서 사용할수 있게 해주는 Hook함수
+    const dispatch = useDispatch();
+    //각 액션을 dispatch하는 함수
+    // dispatch({
+    //     type: "INCREASE"
+    // })
+    const onIncrease = () => dispatch(increase());
+    const onDecrease = () => dispatch(decrease());
+    const onSetDiff = diff => dispatch(setDiff(diff));
+    return (
+        <Counter 
+        number={number} 
+        diff={diff} 
+        onIncrease={onIncrease}
+        onDecrease={onDecrease}
+        onSetDiff={onSetDiff}
+        />
+    );
+};
+
+export default CounterContainer;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
 
+//스토어 만들기
+const store = createStore(rootReducer);
+console.log(store.getState())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
